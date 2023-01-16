ba22ad962a54893be3d3f70ea2850702a036bb36
Söderlund Marcus <marcus.soderlund@softronic.se>
2023-01-16 20:55:06 +0100

    added the 5 brothers

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,55 @@ export default defineConfig({
                                 { name: 'title', widget: 'string', label: 'Titel' },
                                 { name: 'body', widget: 'markdown', label: 'Innehåll' },
                             ]
+                        },
+                        {
+                            name: "slakttrad",
+                            label: "Släktträd",
+                            file: "src/content/slakttrad.md",
+                            fields: [
+                                { name: 'title', widget: 'string', label: 'Titel' },
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
+                        },
+                        {
+                            name: "hans-ake",
+                            label: "Hans-åke",
+                            file: "src/content/hans-ake.md",
+                            fields: [
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
+                        },
+                        {
+                            name: "ingemar",
+                            label: "Ingemar",
+                            file: "src/content/ingemar.md",
+                            fields: [
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
+                        },
+                        {
+                            name: "martin",
+                            label: "Martin",
+                            file: "src/content/martin.md",
+                            fields: [
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
+                        },
+                        {
+                            name: "mats",
+                            label: "Mats",
+                            file: "src/content/mats.md",
+                            fields: [
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
+                        },
+                        {
+                            name: "per",
+                            label: "Per",
+                            file: "src/content/per.md",
+                            fields: [
+                                { name: 'body', widget: 'markdown', label: 'Innehåll' },
+                            ]
                         }]
                     }
                 ],
